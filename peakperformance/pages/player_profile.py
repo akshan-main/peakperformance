ee509dc0b3b7ddae0210d50f848c938e3e1bf0e0
balaji24 <balaji24@uw.edu>
2025-03-20 06:38:53 -0700

    Final changes

--- peakperformance/pages/player_profile.py
+++ peakperformance/pages/player_profile.py
@@ -17,8 +17,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-load_dotenv()
-
 # Global Constants
 CSV_PATH: Path = (
     Path(__file__).resolve().parent.parent.parent
