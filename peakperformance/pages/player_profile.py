36d5173d5af2ed82fbc3159ac516092a41a2f351
balaji24 <balaji24@uw.edu>
2025-03-20 06:29:01 -0700

    Changed dotenv file for streamlit cloud access

--- peakperformance/pages/player_profile.py
+++ peakperformance/pages/player_profile.py
@@ -9,9 +9,7 @@ Date: March 16, 2025
 """
 
 import io
-import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 import streamlit as st
 import requests
@@ -28,8 +26,8 @@ CSV_PATH: Path = (
     / "Ratings Combined"
     / "player_data_with_predictions.csv"
 )
-API_KEY = os.getenv("API_KEY")
-PLAYER_API_URL = f"https://www.thesportsdb.com/api/v1/json/{API_KEY}/searchplayers.php"
+API_KEY: str = "3"  # Free API key from TheSportsDB
+PLAYER_API_URL: str = f"https://www.thesportsdb.com/api/v1/json/{API_KEY}/searchplayers.php"
 
 
 def set_theme() -> None:
